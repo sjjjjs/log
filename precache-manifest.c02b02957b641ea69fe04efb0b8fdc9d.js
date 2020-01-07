0fd1bc27f5b6926bf70ebe2e37e64cf8762360ac
孙俊杰 <junjie.sun@vivo.com>
2020-01-07 19:13:38 +0800

    Updates

---
+++ precache-manifest.c02b02957b641ea69fe04efb0b8fdc9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a568fbb4b4c72ee8fc39f5208628c87",
+    "url": "/log/index.html"
+  },
+  {
+    "revision": "89fa29294c274f0438df",
+    "url": "/log/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f25aa8df7fad7f37917c",
+    "url": "/log/static/js/2.f75fd114.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/log/static/js/2.f75fd114.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89fa29294c274f0438df",
+    "url": "/log/static/js/main.0c88d66c.chunk.js"
+  },
+  {
+    "revision": "eacea376657593114f4c",
+    "url": "/log/static/js/runtime-main.f6d301ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/log/static/media/logo.5d5d9eef.svg"
+  }
+]);
