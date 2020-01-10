46f3f7dd9ab230859be2179f1557a4c4bd5ab37b
孙俊杰 <junjie.sun@vivo.com>
2020-01-10 14:20:26 +0800

    @dev

--- src/component/category/index.js
+++ src/component/category/index.js
@@ -1,53 +1,92 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Tree, Input } from 'antd';
+import Tabulator from 'tabulator-tables';
 
-const { TreeNode } = Tree;
+var tableDataNested = [
+  {name:"Oli Bob", location:"United Kingdom", gender:"male", col:"red", dob:"14/04/1984", _children:[
+      {name:"Mary May", location:"Germany", gender:"female", col:"blue", dob:"14/05/1982"},
+      {name:"Christine Lobowski", location:"France", gender:"female", col:"green", dob:"22/05/1982"},
+      {name:"Brendon Philips", location:"USA", gender:"male", col:"orange", dob:"01/08/1980", _children:[
+          {name:"Margret Marmajuke", location:"Canada", gender:"female", col:"yellow", dob:"31/01/1999"},
+          {name:"Frank Harbours", location:"Russia", gender:"male", col:"red", dob:"12/05/1966"},
+      ]},
+  ]},
+  {name:"Jamie Newhart", location:"India", gender:"male", col:"green", dob:"14/05/1985"},
+  {name:"Gemma Jane", location:"China", gender:"female", col:"red", dob:"22/05/1982", _children:[
+      {name:"Emily Sykes", location:"South Korea", gender:"female", col:"maroon", dob:"11/11/1970"},
+  ]},
+  {name:"James Newman", location:"Japan", gender:"male", col:"red", dob:"22/03/1998"},
+];
 
-export default class Demo extends React.Component {
-  onSelect = (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info);
-  };
+export default function Demo() {
+  let rootRef = null;
+  useEffect(() => {
+    if (rootRef) {
+      new Tabulator(rootRef, {
+        data:tableDataNested,
+        dataTree:true,
+        layout:"fitColumns",
+        headerVisible:false,
+        columns:[
+        {title:"Name", field:"name", responsive:0}, //never hide this column
+        ],
+    });
+    }
+  }, [ rootRef])
+  return (
+    <div ref={ref => rootRef = ref}>
 
-  render() {
-    return (
-      <Tree
-        defaultExpandedKeys={['0-0-0']}
-        onSelect={this.onSelect}
-        blockNode
-        showLine
-      >
-        <div style={{ padding: 4, marginTop: 10 }}>
-          <Input.Search size="small" style={{ marginBottom: 8 }} placeholder="Search" onChange={this.onChange} />
-        </div>
-        <TreeNode title="哲学" key="0-0">
-          <TreeNode title="中国哲学" key="0-0-0">
-            <TreeNode title="儒家" key="0-0-0-0" />
-            <TreeNode title="道家" key="0-0-0-1" />
-            <TreeNode title="佛教" key="0-0-0-2" />
-          </TreeNode>
-          <TreeNode title="西方哲学" key="0-0-1">
-            <TreeNode title="古希腊哲学" key="0-0-1-0" />
-          </TreeNode>
-          <TreeNode title="近代哲学" key="0-0-2">
-            <TreeNode title="存在主义" key="0-0-2-0" />
-            <TreeNode title="花园学派" key="0-0-2-1" />
-          </TreeNode>
-        </TreeNode>
-        <TreeNode title="启蒙运动" key="1-0">
-          <TreeNode title="parent 1-0" key="1-0-0">
-            <TreeNode title="leaf" key="1-0-0-0" />
-            <TreeNode title="leaf" key="1-0-0-1" />
-            <TreeNode title="leaf" key="1-0-0-2" />
-          </TreeNode>
-          <TreeNode title="parent 1-1" key="1-0-1">
-            <TreeNode title="leaf" key="1-0-1-0" />
-          </TreeNode>
-          <TreeNode title="parent 1-2" key="1-0-2">
-            <TreeNode title="leaf" key="1-0-2-0" />
-            <TreeNode title="leaf" key="1-0-2-1" />
-          </TreeNode>
-        </TreeNode>
-      </Tree>
-    );
-  }
+    </div>
+  );
 }
+
+// const { TreeNode } = Tree;
+
+// export default class Demo extends React.Component {
+//   onSelect = (selectedKeys, info) => {
+//     console.log('selected', selectedKeys, info);
+//   };
+
+//   render() {
+//     return (
+//       <Tree
+//         defaultExpandedKeys={['0-0-0']}
+//         onSelect={this.onSelect}
+//         blockNode
+//         showLine
+//       >
+//         <div style={{ padding: 4, marginTop: 10 }}>
+//           <Input.Search size="small" style={{ marginBottom: 8 }} placeholder="Search" onChange={this.onChange} />
+//         </div>
+//         <TreeNode title="哲学" key="0-0">
+//           <TreeNode title="中国哲学" key="0-0-0">
+//             <TreeNode title="儒家" key="0-0-0-0" />
+//             <TreeNode title="道家" key="0-0-0-1" />
+//             <TreeNode title="佛教" key="0-0-0-2" />
+//           </TreeNode>
+//           <TreeNode title="西方哲学" key="0-0-1">
+//             <TreeNode title="古希腊哲学" key="0-0-1-0" />
+//           </TreeNode>
+//           <TreeNode title="近代哲学" key="0-0-2">
+//             <TreeNode title="存在主义" key="0-0-2-0" />
+//             <TreeNode title="花园学派" key="0-0-2-1" />
+//           </TreeNode>
+//         </TreeNode>
+//         <TreeNode title="启蒙运动" key="1-0">
+//           <TreeNode title="parent 1-0" key="1-0-0">
+//             <TreeNode title="leaf" key="1-0-0-0" />
+//             <TreeNode title="leaf" key="1-0-0-1" />
+//             <TreeNode title="leaf" key="1-0-0-2" />
+//           </TreeNode>
+//           <TreeNode title="parent 1-1" key="1-0-1">
+//             <TreeNode title="leaf" key="1-0-1-0" />
+//           </TreeNode>
+//           <TreeNode title="parent 1-2" key="1-0-2">
+//             <TreeNode title="leaf" key="1-0-2-0" />
+//             <TreeNode title="leaf" key="1-0-2-1" />
+//           </TreeNode>
+//         </TreeNode>
+//       </Tree>
+//     );
+//   }
+// }
--- src/component/dataTable/index.js
+++ src/component/dataTable/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Table, Tag } from 'antd';
+import Tabulator from 'tabulator-tables';
 
 import LogComments from '../logComments';
 import LogRemarker from '../logRemarker';
@@ -20,12 +21,10 @@ const columns = [
     title: '分类',
     dataIndex: 'category',
     width: 140,
-    ellipsis: true
   },
   {
     title: '消息',
     dataIndex: 'message',
-    ellipsis: true,
     render: (text, record) => (
       <span>
         <Tag>{record.tag}</Tag>
@@ -38,113 +37,145 @@ const data = [
   {
     key: '1',
     createAt: '01.01 14:09',
-    category: '经济学',
-    tag: '紧急',
-    message: '今天学习了薛兆丰经济学一部分，收益不小'
+    appender: '经济学',
+    level: '紧急',
+    message: '今天学习了薛兆丰经济学一部分，收益不小',
+    _children: [
+      {
+        key: '43',
+        createAt: '01.01 14:09',
+        appender: '减肥计划',
+        level: '铭记',
+        message: '今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！'
+      },
+      {
+        key: '44',
+        createAt: '01.01 14:09',
+        appender: '戒烟',
+        level: '突破',
+        message: '本月累计抽烟 1 根。'
+      },
+    ]
   },
   {
     key: '2',
     createAt: '01.01 14:09',
-    category: '自我管理',
-    tag: '心情',
+    appender: '自我管理',
+    level: '心情',
     message: '我是不是在浪费时间？？'
   },
   {
     key: '3',
     createAt: '01.01 14:09',
-    category: '减肥计划',
-    tag: '铭记',
+    appender: '减肥计划',
+    level: '铭记',
     message: '今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！'
   },
   {
     key: '4',
     createAt: '01.01 14:09',
-    category: '戒烟',
-    tag: '突破',
+    appender: '戒烟',
+    level: '突破',
     message: '本月累计抽烟 1 根。'
   },
   {
     key: '21',
     createAt: '01.01 14:09',
-    category: '经济学',
-    tag: '紧急',
+    appender: '经济学',
+    level: '紧急',
     message: '今天学习了薛兆丰经济学一部分，收益不小'
   },
   {
     key: '22',
     createAt: '01.01 14:09',
-    category: '自我管理',
-    tag: '心情',
-    message: '我是不是在浪费时间？？'
+    appender: '自我管理',
+    level: '心情',
+    message: '我是不是在浪费时间？？',
+    _children: [
+      {
+        key: '43',
+        createAt: '01.01 14:09',
+        appender: '减肥计划',
+        level: '铭记',
+        message: '今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！'
+      },
+      {
+        key: '44',
+        createAt: '01.01 14:09',
+        appender: '戒烟',
+        level: '突破',
+        message: '本月累计抽烟 1 根。'
+      },
+    ]
   },
   {
     key: '23',
     createAt: '01.01 14:09',
-    category: '减肥计划',
-    tag: '铭记',
+    appender: '减肥计划',
+    level: '铭记',
     message: '今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！'
   },
   {
     key: '24',
     createAt: '01.01 14:09',
-    category: '戒烟',
-    tag: '突破',
+    appender: '戒烟',
+    level: '突破',
     message: '本月累计抽烟 1 根。'
   },
   {
     key: '31',
     createAt: '01.01 14:09',
-    category: '经济学',
-    tag: '紧急',
+    appender: '经济学',
+    level: '紧急',
     message: '今天学习了薛兆丰经济学一部分，收益不小'
   },
   {
     key: '32',
     createAt: '01.01 14:09',
-    category: '自我管理',
-    tag: '心情',
+    appender: '自我管理',
+    level: '心情',
     message: '我是不是在浪费时间？？'
   },
   {
     key: '33',
     createAt: '01.01 14:09',
-    category: '减肥计划',
-    tag: '铭记',
+    appender: '减肥计划',
+    level: '铭记',
     message: '今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！'
   },
   {
     key: '34',
     createAt: '01.01 14:09',
-    category: '戒烟',
-    tag: '突破',
+    appender: '戒烟',
+    level: '突破',
     message: '本月累计抽烟 1 根。'
   },
   {
     key: '41',
     createAt: '01.01 14:09',
-    category: '经济学',
-    tag: '紧急',
+    appender: '经济学',
+    level: '紧急',
     message: '今天学习了薛兆丰经济学一部分，收益不小'
   },
   {
     key: '42',
     createAt: '01.01 14:09',
-    category: '自我管理',
-    tag: '心情',
+    appender: '自我管理',
+    level: '心情',
     message: '我是不是在浪费时间？？'
   },
   {
     key: '43',
     createAt: '01.01 14:09',
-    category: '减肥计划',
-    tag: '铭记',
+    appender: '减肥计划',
+    level: '铭记',
     message: '今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！今日达到历史最佳体重 63 公斤！'
   },
   {
     key: '44',
     createAt: '01.01 14:09',
-    category: '戒烟',
-    tag: '突破',
+    appender: '戒烟',
+    level: '突破',
     message: '本月累计抽烟 1 根。'
   },
 ];
@@ -154,23 +185,65 @@ const rowSelection = {
   selectedRowKeys: [ '3' ]
 };
 
+var tableDataNested = [
+  {name:"Oli Bob", location:"United Kingdom", gender:"male", col:"red", dob:"14/04/1984", _children:[
+      {name:"Mary May", location:"Germany", gender:"female", col:"blue", dob:"14/05/1982"},
+      {name:"Christine Lobowski", location:"France", gender:"female", col:"green", dob:"22/05/1982"},
+      {name:"Brendon Philips", location:"USA", gender:"male", col:"orange", dob:"01/08/1980", _children:[
+          {name:"Margret Marmajuke", location:"Canada", gender:"female", col:"yellow", dob:"31/01/1999"},
+          {name:"Frank Harbours", location:"Russia", gender:"male", col:"red", dob:"12/05/1966"},
+      ]},
+  ]},
+  {name:"Jamie Newhart", location:"India", gender:"male", col:"green", dob:"14/05/1985"},
+  {name:"Gemma Jane", location:"China", gender:"female", col:"red", dob:"22/05/1982", _children:[
+      {name:"Emily Sykes", location:"South Korea", gender:"female", col:"maroon", dob:"11/11/1970"},
+  ]},
+  {name:"James Newman", location:"Japan", gender:"male", col:"red", dob:"22/03/1998"},
+];
+
 export default function DataTable(props) {
+  let tableRootRef = null;
+  useEffect(() => {
+    if (tableRootRef) {
+      new Tabulator(tableRootRef, {
+        height:"300px",
+        data:data,
+        // dataTree:true,
+        // movableRows:true,
+        // selectable:true, //make rows selectable
+        // dataTreeStartExpanded:true,
+
+        layout:"fitDataFill",
+        pagination:"local",
+        paginationSize:10,
+        paginationSizeSelector:[3, 6, 8, 10],
+        columns:[
+          {formatter:"rownum", align:"center", width:40},
+          {title:"来源", field:"appender", width:150, responsive:0}, //never hide this column
+          {title:"Example", field:"example", formatter:"textarea", editor:true, },
+          {title:"等级", field:"level", width:150},
+          {title:"记录时间", field:"createAt", align:"center", width:150},
+          {title:"消息", field:"message", responsive:10}, //hide this column first
+        ],
+    })
+    }
+  }, [ tableRootRef ])
   return (
     <div className={styles.tableWrap}>
-      <div className={styles.tableBody}>
-        <Table
+      <div className={styles.tableBody} ref={ref => tableRootRef = ref}>
+        {/* <Table
           className="dataTable"
           showHeader={false}
           dataSource={data}
           columns={columns}
           pagination={false}
           rowSelection={rowSelection}
-        />
+        /> */}
       </div>
       <div className={styles.itemContent}>
-        {/* <div className={styles.commentsContent}>
+        <div className={styles.commentsContent}>
           <LogComments />
-        </div> */}
+        </div>
         <div className={styles.remarkContent}>
           <LogRemarker />
         </div>
