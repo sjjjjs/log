9e5a42b1a6d95bc22c0f9f71a4184dc51e9da78c
孙俊杰 <junjie.sun@vivo.com>
2020-01-11 17:51:13 +0800

    @dev

--- src/component/logTable/index.js
+++ src/component/logTable/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Tag, Tooltip } from '@blueprintjs/core';
+import { Tag, Tooltip, InputGroup } from '@blueprintjs/core';
+import Clock from 'react-live-clock';
 import names from 'classnames';
 
 import styles from './index.module.css';
@@ -17,6 +18,40 @@ export default function LogTable(props) {
                 </div>
             </div>
             <div className={styles.logTableBody}>
+                <div className={styles.logTableRow}>
+                    <div className={names(styles.logTableCol, styles.col1)}>
+                    </div>
+                    <div className={names(styles.logTableCol, styles.col2)}>
+                        <Clock className="bp3-monospace-text" style={{ display: 'flex', alignItems: 'center' }} format={'HH:mm:ss'} ticking={true} timezone={'US/Pacific'} />
+                    </div>
+                    <div className={names(styles.logTableCol, styles.col3)}>
+                        <div className="bp3-select">
+                            <select>
+                                <option selected>Choose Logger</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                                <option value="4">Four</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className={names(styles.logTableCol, styles.col4)}>
+                        <div className="bp3-select">
+                            <select>
+                                <option selected>Choose Level</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                                <option value="4">Four</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className={names(styles.logTableCol, styles.col5)}>
+                        <div style={{ padding: '0 6px' }}>
+                            <InputGroup />
+                        </div>
+                    </div>
+                </div>
                 <div className={styles.logTableRow}>
                     <div className={names(styles.logTableCol, styles.col1)}>1</div>
                     <div className={names(styles.logTableCol, styles.col2)}>
