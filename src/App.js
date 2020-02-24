c926913a8c2e39d17b58ae0f68fa15ebe7eb4aef
孙俊杰 <sunjunjie@corp.netease.com>
2020-02-24 23:40:34 +0800

    fix: 修复问题

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-// import "antd/dist/antd.css";
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
-// import AppFrame from "./component/appFrame";
-// import DataTable from './component/dataTable';
-// import Category from './component/category';
-
-import LogTable from './component/logTable';
-
 function App() {
   return (
-    <LogTable />
-    // <AppFrame
-    //   side={<Category />}
-    //   footer={<span>稍等片刻，正在加载...</span>}
-    // >
-    //   <DataTable />
-    // </AppFrame>
+    <p>hello world</p>
   );
 }
 
--- src/component/appBox/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './index.module.css';
-
-export default function AppBox(props) {
-    return (
-        <div className={styles.appBox}>
-        </div>
-    );
-}
