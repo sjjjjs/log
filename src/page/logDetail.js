44ef55ad80451d8ae362bec7a8b377969b75ad32
孙俊杰 <sunjunjie@corp.netease.com>
2020-02-29 00:57:13 +0800

    feat: 增加局部编辑

--- src/page/logDetail.js
+++ src/page/logDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import {
-    Navbar, NavbarGroup, Button, Classes
+    Navbar, NavbarGroup, Button, Classes, Switch, Drawer, Position, TextArea
 } from '@blueprintjs/core';
 import styles from './logDetail.module.css';
 import AppFrame from 'component/appFrame';
@@ -10,10 +10,45 @@ import logCommentService from 'service/logComment';
 import LogComments from 'component/logComments';
 import { AppToaster } from 'util/toaster';
 import MarkdownPreview from 'component/markdownPreview';
+import { sliceCodeFromSource, replaceCodeFromSource } from 'util/sourcePosUtil';
 
-function PageNavigator() {
+function PartialEditDrawer(props) {
+    const { isOpen = false, onClose, value, onChange } = props;
+    return (
+        <Drawer
+            icon="annotation"
+            autoFocus
+            hasBackdrop
+            isOpen={isOpen}
+            position={Position.RIGHT}
+            size={Drawer.SIZE_STANDARD}
+            onClose={onClose}
+        >
+            <div className={Classes.DRAWER_BODY}>
+                <div className={Classes.DIALOG_BODY}>
+                    <p>
+                        <strong>
+                            局部编辑
+                        </strong>
+                    </p>
+                    <TextArea
+                        fill
+                        large
+                        growVertically
+                        value={value}
+                        onChange={evt => onChange(evt.target.value)}
+                        className={Classes.MONOSPACE_TEXT}
+                    />
+                </div>
+            </div>
+        </Drawer>
+    );
+}
+
+function PageNavigator(props) {
     const h = useHistory();
     const params = useParams();
+    const { partialEdit = false, setPartialEdit } = props;
     return (
         <Navbar className={Classes.DARK}>
             <div style={{width: '640px', margin: 'auto'}}>
@@ -38,6 +73,7 @@ function PageNavigator() {
                             alert(err.message);
                         });
                     }}>删除</Button>
+                    <Switch label="局部修改" checked={partialEdit} onChange={setPartialEdit} />
                 </NavbarGroup>
             </div>
         </Navbar>
@@ -46,8 +82,13 @@ function PageNavigator() {
 
 function LogDetail(props) {
     const params = useParams();
+    const [ flag, setFlag ] = useState(false);
     const [ logData, setLogData ] = useState(null);
     const [ logCommentsData, setLogCommentsData ] = useState([]);
+    const [ partialEdit, setPartialEdit ] = useState(false);
+    const [ isOpenPartialEditDrawer, setIsOpenPartialEditDrawer ] = useState(false);
+    const [ partialSource, setPartialSource ] = useState('');
+    const [ posData, setPosData ] = useState(null);
     useEffect(() => {
         async function fetchData() {
             const logInfo = await logService.get(params.id);
@@ -56,16 +97,19 @@ function LogDetail(props) {
             setLogCommentsData(logCommentsInfo);
         }
         fetchData();
-    }, [ params.id ]);
+    }, [ params.id, flag ]);
     return (
-        <AppFrame header={<PageNavigator />}>
+        <AppFrame header={<PageNavigator partialEdit={partialEdit} setPartialEdit={() => setPartialEdit(!partialEdit)} />}>
             <div className={styles.container}>
                 {
                     logData && <div className={styles.logContentBox}>
                         <div className={styles.logContent}>
-                            <MarkdownPreview source={logData.content} selectAble onSelect={
+                            <MarkdownPreview source={logData.content} selectAble={partialEdit} onSelect={
                                 pos => {
-                                    console.log('pos', pos);
+                                    const partial = sliceCodeFromSource(logData.content, pos);
+                                    setIsOpenPartialEditDrawer(true);
+                                    setPartialSource(partial);
+                                    setPosData(pos);
                                 }
                             } />
                         </div>
@@ -78,6 +122,18 @@ function LogDetail(props) {
                     </div>
                 </div>
             </div>
+            <PartialEditDrawer
+                isOpen={isOpenPartialEditDrawer}
+                onClose={() => {
+                    setIsOpenPartialEditDrawer(false);
+                    const source = replaceCodeFromSource(logData.content, posData, partialSource);
+                    logService
+                        .upd(logData.id, { content: source })
+                        .then(() => setFlag(!flag));
+                }}
+                value={partialSource}
+                onChange={val => setPartialSource(val)}
+            />
         </AppFrame>
     );
 }
